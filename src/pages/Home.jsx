a69f9f391ad0211eed7d924c32fab4c284d72a58
CrepMC <nhan069069@stu.vinschool.edu.vn>
2024-10-24 22:11:53 +0700

    update_5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { Navbar, Main, Product, Footer } from "../components";
 
 function Home() {
-  const [searchResults, setSearchResults] = useState(null);
+  const [searchResults, setSearchResults] = useState(null); // searchResults is used to store the results of the product search
 
   const handleSearch = async (query) => {
     console.log("Search query:", query);
     if (query) {
       const response = await fetch(`https://fakestoreapi.com/products?search=${query}`);
       const results = await response.json();
-      setSearchResults(results);
+      setSearchResults(results); // Update searchResults with the fetched results
     } else {
       setSearchResults(null); // Clear results if the query is empty
     }
@@ -19,7 +19,7 @@ function Home() {
     <>
       <Navbar onSearch={handleSearch} /> {/* Pass the handleSearch function */}
       <Main />
-      <Product />
+      <Product results={searchResults} /> {/* Pass searchResults to Product component */}
       <Footer />
     </>
   );
